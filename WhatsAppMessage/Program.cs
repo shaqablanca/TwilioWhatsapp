6572ce6da4592a021f609fbc6b4ccdef00c59a8e
shaqablanca <a.sameduz@gmail.com>
2023-07-17 00:27:55 +0200

    Twillio small adjustments

--- WhatsAppMessage/Program.cs
+++ WhatsAppMessage/Program.cs
@@ -8,35 +8,40 @@ class Program
 {
     static void Main()
     {
-        const string accountSid = "YOUR_ACCOUNT_SID";
-        const string authToken = "YOUR_AUTH_TOKEN";
+        const string accountSid = "AccountSidFromTwilio";
+        const string authToken = "AccountAuthTokenFromTwilio";
 
         TwilioClient.Init(accountSid, authToken);
 
-        // Specify the WhatsApp group ID and the receiver's phone number
-        string groupId = "YOUR_GROUP_ID";
-        string receiverNumber = "RECEIVER_PHONE_NUMBER";
+        
+        string groupId = "WhatsAppGroupId";  // WhatsApp group ID and 
+        string receiverNumber = "ReceiverNumberWhoIsOutOfGroup";// the receiver's phone number
 
         // Retrieve the group participants
         var participants = ParticipantResource.Read(
             pathConversationSid: groupId,
-            limit: 20 // Adjust the limit if needed
+            limit: 6 
         );
 
-        // Iterate over the participants and send a message to the receiver
+        // Iterate over the participants and send a message
         foreach (var participant in participants)
         {
             // Exclude the receiver from the group message
             if (participant.Address != receiverNumber)
             {
                 Twilio.Rest.Api.V2010.Account.MessageResource.Create(
-                    from: new PhoneNumber("whatsapp:YOUR_PHONE_NUMBER"), // Your Twilio phone number
+                    from: new PhoneNumber("whatsapp:" + participant.Address), // Your Twilio phone number
                     to: new PhoneNumber("whatsapp:" + participant.Address), // Participant's phone number
                     body: "Hello, this is a group message!"
                 );
             }
+
         }
 
-        Console.WriteLine("Group messages sent successfully.");
+
+        Console.WriteLine("End of Process. Check the Group Messages and other Receipient");
     }
+
+
+
 }
