9c890730eda1d7bf5b17a2d1bcd46eaafd881d98
shaqablanca <a.sameduz@gmail.com>
2023-07-17 01:09:53 +0200

    Twillio Account configuration need to be handled

--- WhatsAppMessage/Program.cs
+++ WhatsAppMessage/Program.cs
@@ -6,32 +6,30 @@ using Twilio.Types;
 
 class Program
 {
+    
     static void Main()
     {
-        const string accountSid = "AccountSidFromTwilio";
-        const string authToken = "AccountAuthTokenFromTwilio";
+        const string accountSid = "ACef4....................594283a91";
+        const string authToken = "65c....................df9147a42";
 
         TwilioClient.Init(accountSid, authToken);
-
-        
-        string groupId = "WhatsAppGroupId";  // WhatsApp group ID and 
-        string receiverNumber = "ReceiverNumberWhoIsOutOfGroup";// the receiver's phone number
+        string groupId = "https://chat.whatsapp.com/HlWuCxKgIxm3N1IRXsKn7F";  // WhatsApp group ID and 
+        string receiverNumber = "+905......511";// the receiver's phone number
 
         // Retrieve the group participants
-        var participants = ParticipantResource.Read(
-            pathConversationSid: groupId,
-            limit: 6 
+        var members = ParticipantResource.Read(
+            pathConversationSid: groupId
         );
 
         // Iterate over the participants and send a message
-        foreach (var participant in participants)
+        foreach (var member in members)
         {
             // Exclude the receiver from the group message
-            if (participant.Address != receiverNumber)
+            if (member.Identity != receiverNumber)
             {
                 Twilio.Rest.Api.V2010.Account.MessageResource.Create(
-                    from: new PhoneNumber("whatsapp:" + participant.Address), // Your Twilio phone number
-                    to: new PhoneNumber("whatsapp:" + participant.Address), // Participant's phone number
+                    from: new PhoneNumber("whatsapp:+337......13"), // Your Twilio phone number
+                    to: new PhoneNumber("whatsapp:" + member.Identity), // Participant's phone number
                     body: "Hello, this is a group message!"
                 );
             }
