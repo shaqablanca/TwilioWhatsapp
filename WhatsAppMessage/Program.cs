4537a2d3a55d8ec3843562984064200acad0326f
shaqablanca <a.sameduz@gmail.com>
2023-07-19 16:40:51 +0200

    iterate each messages written in group and send them each to the outerReceiver

--- WhatsAppMessage/Program.cs
+++ WhatsAppMessage/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Twilio;
-using Twilio.Rest.Api.V2010.Account;
 using Twilio.Rest.Conversations.V1.Conversation;
+using Twilio.TwiML.Messaging;
 using Twilio.Types;
 
 class Program
@@ -9,37 +9,36 @@ class Program
     
     static void Main()
     {
-        const string accountSid = "ACef4....................594283a91";
-        const string authToken = "65c....................df9147a42";
+        string accountSid = "ACef4....................594283a91";
+        string authToken = "65c....................df9147a42";
 
         TwilioClient.Init(accountSid, authToken);
         string groupId = "https://chat.whatsapp.com/HlWuCxKg..........Kn7F";  // WhatsApp group ID and 
-        string receiverNumber = "+905......511";// the receiver's phone number
+        string OuterReceiver = "+905......511";// the receiver's phone number
 
         // Retrieve the group participants
-        var members = ParticipantResource.Read(
+        var messages = MessageResource.Read(
             pathConversationSid: groupId
         );
 
-        // Iterate over the participants and send a message
-        foreach (var member in members)
+         
+        // Iterate over the messages and send them  all individually to the OuterReceiver
+        foreach (var message in messages)
         {
-            // Exclude the receiver from the group message
-            if (member.Identity != receiverNumber)
-            {
-                Twilio.Rest.Api.V2010.Account.MessageResource.Create(
-                    from: new PhoneNumber("whatsapp:+337......13"), // Your Twilio phone number
-                    to: new PhoneNumber("whatsapp:" + member.Identity), // Participant's phone number
-                    body: "Hello, this is a group message!"
-                );
-            }
-
+            Twilio.Rest.Api.V2010.Account.MessageResource.Create(
+                from: new PhoneNumber("whatsapp:+337......13"),       // Your Twilio phone number
+                to: new PhoneNumber("whatsapp:" + OuterReceiver),   // Receiver's phone number
+                body: message.Body
+            );
         }
-
-
-        Console.WriteLine("End of Process. Check the Group Messages and other Receipient");
+       
+	        Console.WriteLine("End of Process. Check the Group Messages and other Receipient");
     }
 
 
-
+        
 }
+
+
+
+
